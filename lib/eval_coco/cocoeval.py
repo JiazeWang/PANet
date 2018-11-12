96cc32efacabfb739f505a1db16a2a9299e148c5
JiazeWang <jiazewang@csu.edu.cn>
2018-11-13 04:07:17 +0800

    fix

--- lib/eval_coco/cocoeval.py
+++ lib/eval_coco/cocoeval.py
@@ -153,7 +153,8 @@ class COCOeval:
         IOUSCORE = []
         for catId in catIds:
             for imgId in p.imgIds:
-                a = (computeIoU(imgId, catId))[0][0]
+                #a = (computeIoU(imgId, catId))[0][0]
+                a = (computeIoU(imgId, catId))[0]
                 IOUSCORE.append(str(a))
                 #print(a)
     #    file = open('file_name.txt','w')
