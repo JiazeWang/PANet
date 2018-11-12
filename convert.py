76c99bd912fd897ea1fa4449081828454467e560
JiazeWang <jiazewang@csu.edu.cn>
2018-11-13 01:42:38 +0800

    fix

--- convert.py
+++ convert.py
@@ -66,7 +66,7 @@ def cocoSegmentationToPngDemo(dataDir= data_dir, dataTypeAnn='val2017', dataType
 
     # Convert each image to a png
     imgCount = len(imgIds)
-    for i in xrange(0, imgCount):
+    for i in range(0, imgCount):
         imgId = imgIds[i]
         imgName = coco.loadImgs(ids=imgId)[0]['file_name'].replace('.jpg', '')
         print('Exporting image %d of %d: %s' % (i+1, imgCount, imgName))
